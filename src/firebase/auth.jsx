6cf578725f0e0d78bdd04d53259159185f0447de
Jibon Krishna Roy <mailme.jibon@gmail.com>
2023-10-08 17:35:29 +0600

    Firebase installed and customizing

---
+++ src/firebase/auth.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getAuth } from "firebase/app";
+
+const data = import.meta.env;
+
+const firebaseConfig = {
+    apiKey: data.VITE_apiKey,
+    authDomain: data.VITE_authDomain,
+    projectId: data.VITE_projectId,
+    storageBucket: data.VITE_storageBucket,
+    messagingSenderId: data.VITE_messagingSenderId,
+    appId: data.VITE_appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
